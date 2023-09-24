9772bfe4beec6c56e65b2ba331d28541ac2fb3a7
yy <1036578914@qq.com>
2023-09-24 14:23:47 -0700

    redux setup and api fetching testing

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "./components/header/Header";
-
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+// import Header from "./components/header/Header";
+import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
-  <BrowserRouter>
-  <Header />
-  <Routes>
-      
-  </Routes>
-</BrowserRouter>
+    const dispatch = useDispatch(getApiConfiguration);
+    const {url}= useSelector((state)=>state.home);
+    console.log(url);
+    useEffect(()=>{
+      apiTesting();
+    },[]);
+
+    const apiTesting =()=>{
+      fetchDataFromApi("/movie/popular".then((res)=>{
+        console.log(res);
+      }))
+    };
+    return <div>App</div>
 }
 
 export default App
--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
 
+import { fetchDataFromApi } from "./utils/api";
 import { Provider } from "react-redux";
+import { Store } from "@reduxjs/toolkit";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>
 );
