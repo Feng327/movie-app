e6e0b868371264271c5e3c018d403aeec5dd74de
yy <1036578914@qq.com>
2023-10-13 13:56:04 -0700

    updated the footer content

--- movie-app/src/components/footer/footer.jsx
+++ movie-app/src/components/footer/footer.jsx
@@ -13,12 +13,8 @@ const Footer = () => {
                     <li className="menuItem">Contact Us</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                   This website is built for educational purpose only.<br></br>
+                   FWDP 2023 Yafeng & Yilin
                 </div>
             </ContentWrapper>
         </footer>
